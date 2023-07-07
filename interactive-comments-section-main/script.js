d24369f5036d51282bb1a1e52c1fae2422e90922
MGreenhalgh <greenhalgh.mat@gmail.com>
2023-07-07 01:42:11 +0100

    Interactive comments section update 2

--- interactive-comments-section-main/script.js
+++ interactive-comments-section-main/script.js
@@ -21,86 +21,60 @@ async function populatePage() {
 
 function loadComments() {
     for (var i = 0; i < data.comments.length; i++) {
-        buildComment(data.comments[i]);
+        var commentsHolder = document.getElementById('comments');
+
+        var commentAdd = constructComment(data.comments[i], constructButtons(data.comments[i].user.username, data.comments[i]), data.comments[i].user.username, false);
+        if (data.comments[i].replies.length != 0) commentAdd += loadReplies(data.comments[i]);
+        commentsHolder.innerHTML += commentAdd;
     }
-}
-
-function buildComment(comment) {
-    var commentsHolder = document.getElementById('comments');
-
-    var commentButtons;
-    var you;
-
-    if (comment.user.username == currentUser.username) {
-        you = " you";
-        commentButtons = "<button type='button' class='deleteButton'> <img src='images/icon-delete.svg' alt=''>Delete</button>" +
-            "<button type='button' class='editButton'> <img src='images/icon-edit.svg' alt=''>Edit</button>";
-    }
-    else {
-        commentButtons = "<button type='button' class='replyButton'> <img src='images/icon-reply.svg' alt=''>Reply</button>";
-    }
-
-    var commentAdd =
-        "<div class='commentBox' id='" + "comment" + comment.id + "'>" +
-        "<div class='voteHolder'>" +
-        "<img class='votePlus' onclick='plusScore(" + comment.id + ", event)' src='images/icon-plus.svg'>" +
-        "<div class='voteScore'>" + comment.score + "</div>" +
-        "<img class='voteMinus'onclick='minusScore(" + comment.id + ", event)' src='images/icon-minus.svg'>" +
-        "</div>" +
-        "<div class='commentBoxMain'>" +
-        "<div class='commentBoxHeader'>" +
-        "<img src='" + comment.user.image.png + "' alt='' class='commentUserImg'>" +
-        "<span class='commentUserName" + you + "'>" + comment.user.username + "</span>" +
-        "<span class='commentAge'>" + comment.createdAt + "</span>" +
-        commentButtons +
-        "</div>" +
-        "<div class='commentText'>" + comment.content + "</div>" +
-        "</div>" +
-        "</div>";
-
-    if (comment.replies.length != 0) commentAdd += loadReplies(comment);
-
-    commentsHolder.innerHTML += commentAdd;
 }
 
 function loadReplies(comment) {
     var repliesadd = "";
 
-    var commentButtons;
-    var you;
-
     for (var i = 0; i < comment.replies.length; i++) {
-
-        if (comment.replies[i].user.username == currentUser.username) {
-            you = " you";
-            commentButtons = "<button type='button' class='deleteButton'> <img src='images/icon-delete.svg' alt=''>Delete</button>" +
-                "<button type='button' class='editButton'> <img src='images/icon-edit.svg' alt=''>Edit</button>";
-        }
-        else {
-            commentButtons = "<button type='button' class='replyButton'> <img src='images/icon-reply.svg' alt=''>Reply</button>";
-        }
-
-        repliesadd +=
-            "<div class='commentBox reply' id='" + "comment" + comment.replies[i].id + "'>" +
-            "<div class='voteHolder'>" +
-            "<img class='votePlus' onclick='plusScore(" + comment.replies[i].id + ", event)' src='images/icon-plus.svg'>" +
-            "<div class='voteScore'>" + comment.replies[i].score + "</div>" +
-            "<img class='voteMinus'onclick='minusScore(" + comment.id + ", event)' src='images/icon-minus.svg'>" +
-            "</div>" +
-            "<div class='commentBoxMain'>" +
-            "<div class='commentBoxHeader'>" +
-            "<img src='" + comment.replies[i].user.image.png + "' alt='' class='commentUserImg'>" +
-            "<span class='commentUserName" + you + "'>" + comment.replies[i].user.username + "</span>" +
-            "<span class='commentAge'>" + comment.replies[i].createdAt + "</span>" +
-            commentButtons +
-            "</div>" +
-            "<div class='commentText'>" + comment.replies[i].content + "</div>" +
-            "</div>" +
-            "</div>";
+        repliesadd += constructComment(comment.replies[i], constructButtons(comment.replies[i].user.username, comment.replies[i]), comment.replies[i].user.username, true);
     }
-
     return repliesadd;
+}
 
+function constructButtons(username, comment) {
+    var commentButtons;
+    if (username == currentUser.username) {
+        commentButtons = "<button type='button' class='deleteButton' onclick='startDelete(event, comment" + comment.id + ")'>Delete</button>" +
+            "<button type='button' class='editButton' onclick='startEdit(event, comment" + comment.id + ")'>Edit</button>";
+    }
+    else {
+        commentButtons = "<button type='button' class='replyButton' onclick='showReply(comment" + comment.id + ")'>Reply</button>";
+    }
+    return commentButtons;
+}
+function constructComment(comment, commentButtons, username, reply) {
+    if (reply) reply = "reply"; else reply = "";
+    if (username == currentUser.username) username = " you";
+    var constructedComment =
+        "<div class='commentBox " + reply + "' id='" + "comment" + comment.id + "'>" +
+        "<div class='voteHolder'>" +
+        "<button type='button' class='votePlus' onclick='voteScore(" + comment.id + ", event, \"plus\" )'></button>" +
+        "<div class='voteScore'>" + comment.score + "</div>" +
+        "<button type='button' class='voteMinus' onclick='voteScore(" + comment.id + ", event, \"minus\" )'></button>" +
+        "</div>" +
+        "<div class='commentBoxMain'>" +
+        "<div class='commentBoxHeader'>" +
+        "<img src='" + comment.user.image.png + "' alt='' class='commentUserImg'>" +
+        "<span class='commentUserName" + username + "'>" + comment.user.username + "</span>" +
+        "<span class='commentAge'>" + comment.createdAt + "</span>" +
+        commentButtons +
+        "</div>" +
+        "<div class='commentText'>" + comment.content + "</div>" +
+        "</div>" +
+        "</div>" +//Start reply
+        "<div id='comment" + comment.id + "ReplyBox" + "'class='replyBox reply hide'>" +
+        "<img src='" + currentUser.image.png + "' class='replyUserImg'>" +
+        "<textarea name='replyInput' class='replyInput' id='replyInputcomment" + comment.id + "' placeholder='Add a reply...'></textarea>" +
+        "<button type='button' class='sendReplyButton' onclick='submitReply(event, comment" + comment.id + ")'>REPLY</button>" +
+        "</div>";
+    return constructedComment;
 }
 
 function loadCurrentUser() {
@@ -108,30 +82,135 @@ function loadCurrentUser() {
     document.getElementById('selfCommentUserImg').src = currentUser.image.png;
 }
 
-function plusScore(comment, event) {
-    var c = Array.from(data.comments).find(check => check.id == comment);
-    const score = c.score;
-    if (event.target.nextElementSibling.innerHTML == score + 1) {
-        event.target.nextElementSibling.innerHTML = score;
-        event.target.style.filter = "brightness(1)";
+function voteScore(comment, event, vote) {
+    var score;
+    for (var i = 0; i < data.comments.length; i++) {
+        if (data.comments[i].id == comment) { score = data.comments[i].score; }
+        if (data.comments[i].replies.length != 0)
+            for (var v = 0; v < data.comments[i].replies.length; v++) if (data.comments[i].replies[v].id == comment) { score = data.comments[i].replies[v].score; }
     }
-    if (event.target.nextElementSibling.innerHTML == score || event.target.nextElementSibling.innerHTML == score - 1) {
-        if (event.target.nextElementSibling.innerHTML == score) event.target.nextElementSibling.innerHTML = score + 1;
-        else if (event.target.nextElementSibling.innerHTML == score - 1) event.target.nextElementSibling.innerHTML = score + 1;
-        event.target.style.filter = "brightness(0.7)";
+    if (vote == "plus") {
+        if (event.target.nextElementSibling.innerHTML == score + 1) {
+            event.target.nextElementSibling.innerHTML = score;
+            event.target.style.filter = "brightness(1)";
+            return;
+        }
+        if (event.target.nextElementSibling.innerHTML == score || event.target.nextElementSibling.innerHTML == score - 1) {
+            if (event.target.nextElementSibling.innerHTML == score) event.target.nextElementSibling.innerHTML = score + 1;
+            else if (event.target.nextElementSibling.innerHTML == score - 1) {
+                event.target.nextElementSibling.innerHTML = score + 1;
+                event.target.nextElementSibling.nextElementSibling.style.filter = "brightness(1)";
+            }
+            event.target.style.filter = "brightness(0.7)";
+        }
+    }
+    if (vote == "minus") {
+
+        if (event.target.previousElementSibling.innerHTML == score - 1) {
+            event.target.previousElementSibling.innerHTML = score;
+            event.target.style.filter = "brightness(1)";
+            return;
+        }
+        if (event.target.previousElementSibling.innerHTML == score || event.target.previousElementSibling.innerHTML == score + 1) {
+            if (event.target.previousElementSibling.innerHTML == score) event.target.previousElementSibling.innerHTML = score - 1;
+            else if (event.target.previousElementSibling.innerHTML == score + 1) {
+                event.target.previousElementSibling.innerHTML = score - 1;
+                event.target.previousElementSibling.previousElementSibling.style.filter = "brightness(1)";
+            }
+            event.target.style.filter = "brightness(0.7)";
+        }
     }
 }
 
-function minusScore(comment, event) {
-    var c = Array.from(data.comments).find(check => check.id == comment);
-    const score = c.score;
-    if (event.target.previousElementSibling.innerHTML == c.score - 1) {
-        event.target.previousElementSibling.innerHTML = score;
-        event.target.style.filter = "brightness(1)";
+function addSelfComment() {
+    var comment = document.getElementById("selfCommentInput").value;
+    if (comment) {
+        console.log(comment);
+        var highestCommentId = 0;
+        for (var i = 0; i < data.comments.length; i++) {
+            if (data.comments[i].id > highestCommentId) highestCommentId = data.comments[i].id;
+            if (data.comments[i].replies.length != 0)
+                for (var v = 0; v < data.comments[i].replies.length; v++) if (data.comments[i].replies[v].id > highestCommentId) highestCommentId = data.comments[i].replies[v].id
+        }
+
+        var newComment = {
+            "id": highestCommentId + 1,
+            "content": comment.toString(),
+            "createdAt": "Now",
+            "score": 0,
+            "user": currentUser
+        };
+
+        document.getElementById('comments').innerHTML += constructComment(newComment, constructButtons(currentUser.username, newComment), currentUser.username, false);
+        document.getElementById("selfCommentInput").value = "";
+
+        if (document.getElementById(document.getElementById("selfCommentInput").id + "error")) {
+            document.getElementById(document.getElementById("selfCommentInput").id + "error").remove();
+            document.getElementById("selfCommentInput").style.borderColor = "#e0e0e0";
+        }
+
     }
-    if (event.target.previousElementSibling.innerHTML == score || event.target.previousElementSibling.innerHTML == score + 1) {
-        if (event.target.previousElementSibling.innerHTML == score) event.target.previousElementSibling.innerHTML = score - 1;
-        else if (event.target.previousElementSibling.innerHTML == score + 1) event.target.previousElementSibling.innerHTML = score - 1;
-        event.target.style.filter = "brightness(0.7)";
+    else {
+        var f = document.getElementById("selfCommentInput");
+        if (!document.getElementById(f.id + "error")) {
+            var e = document.createElement("div");
+            e.innerText = "Cannot be empty";
+            e.classList.add("error");
+            e.style.margin = "80px 0 0 60px";
+            e.style.display = "block";
+            e.id = f.id + "error";
+            f.parentElement.appendChild(e);
+            f.style.borderColor = "red";
+        }
+    }
+
+}
+
+function showReply(comment) {
+    const replyBox = document.getElementById(comment.id + "ReplyBox");
+    if (replyBox.classList.contains("hide")) { replyBox.classList.remove("hide"); replyBox.style.width = comment.style.width }
+    else replyBox.classList.add("hide");
+
+}
+
+function submitReply(event, comment) {
+    var reply = document.getElementById('replyInput' + comment.id).value;
+    if (reply) {
+        var highestCommentId = 0;
+        for (var i = 0; i < data.comments.length; i++) {
+            if (data.comments[i].id > highestCommentId) highestCommentId = data.comments[i].id;
+            if (data.comments[i].replies.length != 0)
+                for (var v = 0; v < data.comments[i].replies.length; v++) if (data.comments[i].replies[v].id > highestCommentId) highestCommentId = data.comments[i].replies[v].id
+        }
+
+        var newReply = {
+            "id": highestCommentId + 1,
+            "content": reply.toString(),
+            "createdAt": "Now",
+            "score": 0,
+            "user": currentUser
+        };
+
+        //MAKE A REPLY HAPPEN
+
+        if (document.getElementById(document.getElementById('replyInput' + comment.id).id + "error")) {
+            document.getElementById(document.getElementById('replyInput' + comment.id).id + "error").remove();
+            document.getElementById('replyInput' + comment.id).style.borderColor = "#e0e0e0";
+        }
+        document.getElementById('replyInput' + comment.id).value = "";
+        showReply(comment);
+    }
+    else {
+        var f = document.getElementById('replyInput' + comment.id);
+        if (!document.getElementById(f.id + "error")) {
+            var e = document.createElement("div");
+            e.innerText = "Cannot be empty";
+            e.classList.add("error");
+            e.style.margin = "80px 0 0 60px";
+            e.style.display = "block";
+            e.id = f.id + "error";
+            f.parentElement.appendChild(e);
+            f.style.borderColor = "red";
+        }
     }
 }
