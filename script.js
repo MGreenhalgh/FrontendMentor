4d028bf1d369da0e7bca5592ce205c8c9ae31928
MGreenhalgh <greenhalgh.mat@gmail.com>
2023-07-27 21:25:25 +0100

    Job listing filtering complete

--- script.js
+++ script.js
@@ -24,3 +24,31 @@ function changeDisplayMode() {
         isDarkMode = true;
     }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    populatePage() //Populating the page after waiting for the JSON data to load
+    //Do stuff after loading from JSON
+});
+async function fetchJSON() { //Load all the JSON data and return after it's all loaded
+    var r = await fetch('data.json');
+    var d = await r.json();
+    return data = d;
+}
+async function populatePage() {
+    var wait = await fetchJSON(); //Wait for all the JSON data to load from this function before we do anything else
+    if (data) console.log('JSON data loaded');
+
+    var projectHolder = document.getElementById('projectHolder');
+
+    for (var i = 0; i < data.projects.length; i++) {
+        var newHTML =
+            "<div class='project'>" +
+            "<img src='" + data.projects[i].path + "/design/desktop-design.jpg' alt='" + data.projects[i].name + "'>" +
+            "<div class='projectLinkHolder'>" +
+            "<a class='projectLink' href='" + data.projects[i].path + "'>" + data.projects[i].name + "</a>" +
+            "<a class='FMLink' href='" + data.projects[i].url + "'>View on Frontend Mentor</a>" +
+            "</div>"
+        "</div>"
+        projectHolder.innerHTML += newHTML;
+    }
+}
